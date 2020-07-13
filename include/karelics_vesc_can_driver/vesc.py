a7cd99425a2e7d6fd4f56e263990b3a5a4b32bfd
mart <mart@mmoerdijk.nl>
2020-07-13 20:23:05 +0200

    Restructured driver by splitting it up in multiple files
    Vesc messages and vesc interface are now modules

---
+++ include/karelics_vesc_can_driver/vesc.py
@@ -0,0 +1,110 @@
+
+import rospy
+
+from std_msgs.msg import Float32
+
+from karelics_vesc_can_driver.msg import VescStatus
+
+from karelics_vesc_can_driver.vesc_messages import *
+
+
+class Vesc:
+
+    def __init__(self, vesc_id, send_function):
+        self.vesc_id = vesc_id
+
+        self.motor_poles = 8
+
+        # Status message
+        self.erpm = 0
+        self.duty_cycle = 0
+        self.current = 0
+        # Status message 2
+        self.amp_hours = 0
+        self.amp_hours_charged = 0
+        # Status message 3
+        self.watt_hours = 0
+        self.watt_hours_charged = 0
+        # Status message 4
+        self.temp_fet = 0
+        self.temp_motor = 0
+        self.current_in = 0
+        self.pid_pos_now = 0
+        # Status message 5
+        self.tacho_value = 0
+        self.v_in = 0
+
+        # Subscribe to cmd topics
+        rospy.Subscriber("vesc_%i/current" % self.vesc_id, Float32, self.set_current_cb)
+        rospy.Subscriber("vesc_%i/brake_current" % self.vesc_id, Float32, self.set_brake_cb)
+        rospy.Subscriber("vesc_%i/duty_cycle" % self.vesc_id, Float32, self.set_duty_cycle_cb)
+        rospy.Subscriber("vesc_%i/position" % self.vesc_id, Float32, self.set_position_cb)
+        rospy.Subscriber("vesc_%i/erpm" % self.vesc_id, Float32, self.set_erpm_cb)
+        rospy.Subscriber("vesc_%i/rpm" % self.vesc_id, Float32, self.set_rpm_cb)
+
+
+        # Setup vesc status publischer
+        self.status_pub = rospy.Publisher("vesc_%i/status" % self.vesc_id, VescStatus, queue_size=1)
+
+        self.send_cb = send_function
+
+    def publish_status(self):
+
+        status_msg = VescStatus()
+        status_msg.erpm = int(self.erpm)
+        status_msg.rpm = int(self.erpm / self.motor_poles)
+        status_msg.duty_cycle = self.duty_cycle
+        status_msg.current = self.current
+        status_msg.amp_hours = self.amp_hours
+        status_msg.amp_hours_charged = self.amp_hours_charged
+        status_msg.watt_hours = self.watt_hours
+        status_msg.watt_hours_charged = self.watt_hours_charged
+        status_msg.temp_fet = self.temp_fet
+        status_msg.temp_motor = self.temp_motor
+        status_msg.current_in = self.current_in
+        status_msg.pid_pos_now = self.pid_pos_now
+        status_msg.tacho_value = self.tacho_value
+        status_msg.v_in = self.v_in
+
+        self.status_pub.publish(status_msg)
+
+    def set_send_cb(self, send_function):
+        self.send_cb = send_function
+
+    def __str__(self):
+        string = "VESC[%i]: \n" % self.vesc_id
+        string += "erpm: %i duty: %f  current: %i  \n" % (self.erpm, self.duty_cycle, self.current)
+        string += "amp_hours: %i amp_hours_charged: %f \n" % (self.amp_hours, self.amp_hours_charged)
+        string += "watt_hours: %i watt_hours_charged: %f \n" % (self.watt_hours, self.watt_hours_charged)
+        string += "temp_fet: %f temp_motor: %f current_in: %f " \
+                  "pid_pos_now: %f  \n" % (self.temp_fet, self.temp_motor, self.current_in, self.pid_pos_now)
+        string += "tacho_value: %f v_in: %f \n" % (self.tacho_value, self.v_in)
+        return string
+
+    def set_current_cb(self, msg: Float32):
+        current_msg = VesSetCurrent(current=msg.data)
+        self.send_cb(current_msg.get_can_msg(self.vesc_id))
+        pass
+
+    def set_brake_cb(self, msg: Float32):
+        brake_current_msg = VesSetBrakeCurrent(current=msg.data)
+        self.send_cb(brake_current_msg.get_can_msg(self.vesc_id))
+        pass
+
+    def set_duty_cycle_cb(self, msg: Float32):
+        dutycyle_msg = VesSetDuty(dutycycle=msg.data)
+        self.send_cb(dutycyle_msg.get_can_msg(self.vesc_id))
+
+    def set_position_cb(self, msg: Float32):
+        pos_msg = VesSetPos(pos=msg.data)
+        self.send_cb(pos_msg.get_can_msg(self.vesc_id))
+
+    def set_erpm_cb(self, msg: Float32):
+        rpm_msg = VesSetRPM(rpm=msg.data)
+        self.send_cb(rpm_msg.get_can_msg(self.vesc_id))
+        pass
+
+    def set_rpm_cb(self, msg: Float32):
+        rpm_msg = VesSetRPM(rpm=msg.data*self.motor_poles)
+        self.send_cb(rpm_msg.get_can_msg(self.vesc_id))
+        pass
---
+++ include/karelics_vesc_can_driver/vesc_messages.py
@@ -0,0 +1,278 @@
+from abc import ABCMeta
+from enum import IntEnum
+
+from karelics_vesc_can_driver.vesc import Vesc
+
+
+class CanIds(IntEnum):
+    CAN_PACKET_SET_DUTY = 0
+    CAN_PACKET_SET_CURRENT = 1
+    CAN_PACKET_SET_CURRENT_BRAKE = 2
+    CAN_PACKET_SET_RPM = 3
+    CAN_PACKET_SET_POS = 4
+    CAN_PACKET_FILL_RX_BUFFER = 5
+    CAN_PACKET_FILL_RX_BUFFER_LONG = 6
+    CAN_PACKET_PROCESS_RX_BUFFER = 7
+    CAN_PACKET_PROCESS_SHORT_BUFFER = 8
+    CAN_PACKET_STATUS = 9
+    CAN_PACKET_SET_CURRENT_REL = 10
+    CAN_PACKET_SET_CURRENT_BRAKE_REL = 11
+    CAN_PACKET_SET_CURRENT_HANDBRAKE = 12
+    CAN_PACKET_SET_CURRENT_HANDBRAKE_REL = 13
+    CAN_PACKET_STATUS_2 = 14
+    CAN_PACKET_STATUS_3 = 15
+    CAN_PACKET_STATUS_4 = 16
+    CAN_PACKET_PING = 17
+    CAN_PACKET_PONG = 18
+    CAN_PACKET_DETECT_APPLY_ALL_FOC = 19
+    CAN_PACKET_DETECT_APPLY_ALL_FOC_RES = 20
+    CAN_PACKET_CONF_CURRENT_LIMITS = 21
+    CAN_PACKET_CONF_STORE_CURRENT_LIMITS = 22
+    CAN_PACKET_CONF_CURRENT_LIMITS_IN = 23
+    CAN_PACKET_CONF_STORE_CURRENT_LIMITS_IN = 24
+    CAN_PACKET_CONF_FOC_ERPMS = 25
+    CAN_PACKET_CONF_STORE_FOC_ERPMS = 26
+    CAN_PACKET_STATUS_5 = 27
+    CAN_PACKET_POLL_TS5700N8501_STATUS = 28
+    CAN_PACKET_CONF_BATTERY_CUT = 29
+    CAN_PACKET_CONF_STORE_BATTERY_CUT = 30
+    CAN_PACKET_SHUTDOWN = 31
+
+
+
+class CanMsg(metaclass=ABCMeta):
+
+    def __init__(self, msg_id):
+        self.msg_id = msg_id
+        self.in_buffer = bytes()
+        self.out_buffer = bytearray()
+        self.pointer = 0
+        self.on_update_func = None
+
+    def pop_int32(self):
+        data = struct.unpack(">i", self.in_buffer[self.pointer:self.pointer + 4])[0]
+        self.pointer += 4
+        return data
+
+    def pop_int16(self):
+        data = struct.unpack(">h", self.in_buffer[self.pointer:self.pointer + 2])[0]
+        self.pointer += 2
+        return data
+
+    def set_data(self, data: bytes):
+        self.pointer = 0
+        self.in_buffer = data
+
+    def process_msg(self, data):
+        pass
+
+    def start_msg(self):
+        self.out_buffer = bytearray()
+
+    def encode_int32(self, value):
+        for b in struct.pack(">i", value):
+            self.out_buffer.append(b)
+
+    def encode_uint32(self, value):
+        for b in struct.pack(">I", value):
+            self.out_buffer.append(b)
+
+    def encode_float32(self, value):
+        for b in struct.pack(">f", value):
+            self.out_buffer.append(b)
+
+    def get_encoded_msg_id(self, controller_id):
+
+        return controller_id | (self.msg_id << 8)
+
+    def get_can_msg(self, vesc_id):
+
+        can_msg = Frame()
+        can_msg.is_extended = True
+        can_msg.id = self.get_encoded_msg_id(vesc_id)
+        can_msg.data = self.get_encoded_msg()
+        can_msg.dlc = len(self.out_buffer)
+
+        return can_msg
+
+    def get_encoded_msg(self):
+        pass
+
+
+class VescStatusMsg(CanMsg):
+
+    def __init__(self):
+        super(VescStatusMsg, self).__init__(msg_id=CanIds.CAN_PACKET_STATUS)
+        self.erpm = 0
+        self.current = 0
+        self.duty_cycle = 0
+
+    def process_msg(self, data):
+        self.set_data(data)
+        self.erpm = self.pop_int32()
+        self.current = self.pop_int16() / 10.0
+        self.duty_cycle = self.pop_int16() / 1000.0
+        return self
+
+    def update_vesc_state(self, vesc: Vesc):
+        vesc.erpm = self.erpm
+        vesc.current = self.current
+        vesc.duty_cycle = self.duty_cycle
+
+
+class VescStatus2Msg(CanMsg):
+
+    def __init__(self):
+
+        super(VescStatus2Msg, self).__init__(msg_id=CanIds.CAN_PACKET_STATUS_2)
+        self.amp_hours = 0
+        self.amp_hours_charged = 0
+
+    def update_vesc_state(self, vesc: Vesc):
+        vesc.amp_hours = self.amp_hours
+        vesc.amp_hours_charged = self.amp_hours_charged
+
+    def process_msg(self, data):
+        self.set_data(data)
+        self.amp_hours = self.pop_int32()/1e4
+        self.amp_hours_charged = self.pop_int32()/1e4
+        return self
+
+
+class VescStatus3Msg(CanMsg):
+
+    def __init__(self):
+
+        super(VescStatus3Msg, self).__init__(msg_id=CanIds.CAN_PACKET_STATUS_3)
+        self.watt_hours = 0
+        self.watt_hours_charged = 0
+
+    def update_vesc_state(self, vesc: Vesc):
+        vesc.watt_hours = self.watt_hours
+        vesc.watt_hours_charged = self.watt_hours_charged
+
+    def process_msg(self, data):
+        self.set_data(data)
+        self.watt_hours = self.pop_int32()/1e4
+        self.watt_hours_charged = self.pop_int32()/1e4
+        return self
+
+
+class VescStatus4Msg(CanMsg):
+
+    def __init__(self):
+
+        super(VescStatus4Msg, self).__init__(msg_id=CanIds.CAN_PACKET_STATUS_4)
+        self.temp_fet = 0
+        self.temp_motor = 0
+        self.current_in = 0
+        self.pid_pos_now = 0
+
+    def update_vesc_state(self, vesc: Vesc):
+        vesc.temp_fet = self.temp_fet
+        vesc.temp_motor = self.temp_motor
+        vesc.current_in = self.current_in
+        vesc.pid_pos_now = self.pid_pos_now
+
+    def process_msg(self, data):
+        self.set_data(data)
+        self.temp_fet = self.pop_int16() / 10.0
+        self.temp_motor = self.pop_int16() / 10.0
+        self.current_in = self.pop_int16() / 10.0
+        self.pid_pos_now = self.pop_int16() / 50.0
+        return self
+
+
+class VescStatus5Msg(CanMsg):
+
+    def __init__(self):
+
+        super(VescStatus5Msg, self).__init__(msg_id=CanIds.CAN_PACKET_STATUS_5)
+        self.tacho_value = 0
+        self.v_in = 0
+
+    def update_vesc_state(self, vesc: Vesc):
+        vesc.tacho_value = self.tacho_value
+        vesc.v_in = self.v_in
+
+    def process_msg(self, data):
+        self.set_data(data)
+        self.tacho_value = self.pop_int32()
+        self.v_in = self.pop_int16() / 1e1
+        return self
+
+
+class VesSetDuty(CanMsg):
+
+    def __init__(self, dutycycle=0):
+        super(VesSetDuty, self).__init__(msg_id=CanIds.CAN_PACKET_SET_DUTY)
+        self.duty_cycle = dutycycle
+
+    def get_encoded_msg(self):
+        self.start_msg()
+        self.encode_int32(int(self.duty_cycle*100000))
+        return self.out_buffer
+
+
+class VesSetCurrent(CanMsg):
+
+    def __init__(self, current=0):
+        super(VesSetCurrent, self).__init__(msg_id=CanIds.CAN_PACKET_SET_CURRENT)
+        self.current = current
+
+    def get_encoded_msg(self):
+        self.start_msg()
+        self.encode_int32(int(self.current*1000))
+        return self.out_buffer
+
+
+class VesSetBrakeCurrent(CanMsg):
+
+    def __init__(self, current=0):
+        super(VesSetBrakeCurrent, self).__init__(msg_id=CanIds.CAN_PACKET_SET_CURRENT_BRAKE)
+        self.current = current
+
+    def get_encoded_msg(self):
+        self.start_msg()
+        self.encode_int32(int(self.current*1000))
+        return self.out_buffer
+
+
+class VesSetRPM(CanMsg):
+
+    def __init__(self, rpm):
+        super(VesSetRPM, self).__init__(msg_id=CanIds.CAN_PACKET_SET_RPM)
+        self.rpm = rpm
+
+    def get_encoded_msg(self):
+        self.start_msg()
+        self.encode_int32(int(self.rpm))
+        return self.out_buffer
+
+
+class VesSetPos(CanMsg):
+
+    def __init__(self, pos=0):
+        super(VesSetPos, self).__init__(msg_id=CanIds.CAN_PACKET_SET_POS)
+        self.pos = pos
+
+    def get_encoded_msg(self):
+        self.start_msg()
+        self.encode_int32(int(self.pos))
+        return self.out_buffer
+
+
+class VesSetCurrentRel(CanMsg):
+    """
+    Set current relative to the minimum and maximum current limits.
+    range [-1.0 1.0]
+    """
+
+    def __init__(self, current=0):
+        super(VesSetCurrentRel, self).__init__(msg_id=CanIds.CAN_PACKET_SET_CURRENT_REL)
+        self.current = current
+
+    def get_encoded_msg(self):
+        self.start_msg()
+        self.encode_float32(int(self.current))
+        return self.out_buffer
