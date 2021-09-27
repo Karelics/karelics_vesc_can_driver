b7a61cd8b2e52b792b4ec7bd00357515ad06bc0a
George Porusniuc <george.porusniuc@karelics.fi>
2021-09-27 14:08:01 +0300

    ongoing changes

--- karelics_vesc_can_driver/battery_status.py
+++ karelics_vesc_can_driver/battery_status.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from functools import partial
 import re
 import sys
 
@@ -6,18 +7,18 @@ import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import BatteryState
 
+from karelics_vesc_can_driver.msg import VescStatus
+
 
 class BatteryStatus(Node):
     def __init__(self):
         super().__init__("battery_status")
 
         self.battery_pub = self.create_publisher(BatteryState, "/battery", qos_profile=1)
-
         self.battery_pub_timer = self.create_timer(1.0, self.publish_battery_percentage)
 
-        self.vesc_status_topics = []
-        self.vesc_status_subscribers = []
-        self.vesc_voltages = []
+        self.vesc_status_subscribers = dict()
+        self.vesc_voltages = dict()
 
         self.current_battery_voltage = None
 
@@ -30,22 +31,55 @@ class BatteryStatus(Node):
                              [41.8, 0.02],
                              [40, 0]]
 
-    def get_vesc_status_subscribers(self):
+    def vesc_status_cb(self, data, vesc):
+        # TODO
+        return
+
+    def get_vesc_status_topics(self):
         topics_and_types = self.get_publisher_names_and_types_by_node('karelics_vesc_can_driver', '/', no_demangle=False)
-        status_topics = []
-        string_topic_format = re.compile(r'/vesc_(\d+)/status')
+        vesc_status_topics = []
+        string_topic_format = re.compile(r'/vesc_(\d+)/status')  # RegEx template to find all existing vesc status topics
 
         for topic_tuple in topics_and_types:
             if string_topic_format.match(topic_tuple[0]):
-                status_topics.append(topic_tuple[0])
+                vesc_status_topics.append(topic_tuple[0])
 
-        print()
-        print(status_topics)
-        print()
+        return vesc_status_topics
+
+    @staticmethod
+    def vesc_from_topic(topic):
+        return list(filter(None, topic.split(sep='/')))[0]
+
+    def get_vesc_status_subscribers(self, topics_list):
+        # no new subscribers to add
+        if len(self.vesc_status_subscribers) == len(topics_list):
+            return
+
+        # create all the subs
+        if len(self.vesc_status_subscribers) == 0:
+            for topic in topics_list:
+                vesc_from_current_topic = self.vesc_from_topic(topic)
+                status_sub = self.create_subscription(VescStatus, topic, partial(self.vesc_status_cb,
+                                                                                 vesc=vesc_from_current_topic),
+                                                      qos_profile=1)
+                self.vesc_status_subscribers[vesc_from_current_topic] = status_sub
+
+        # we have new vescs, create new subs
+        if len(self.vesc_status_subscribers) < len(topics_list):
+            # TODO
+            return
+
+        # we have less vescs, remove/destroy old subs
+        if len(self.vesc_status_subscribers) > len(topics_list):
+            # TODO
+            return
 
     def publish_battery_percentage(self):
         # get vesc status topics. If there are new ones, register subs to them and get the data
-        self.get_vesc_status_subscribers()
+        vesc_status_topics = self.get_vesc_status_topics()
+        self.get_vesc_status_subscribers(vesc_status_topics)
+
+        print(self.vesc_status_subscribers)
 
         battery_state = BatteryState()
         battery_state.voltage = 40.0  # temporary battery voltage placeholder
