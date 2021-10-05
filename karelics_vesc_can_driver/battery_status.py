f5c6cad871bc3afe16f5ce8cc1029a020e7643ce
George Porusniuc <george.porusniuc@karelics.fi>
2021-10-05 15:22:22 +0300

    Changes concerning battery_status.py: * cast to float explicitly only in returns in get_battery_percentage * made key_voltages as class variable and a tuple of tuples instead of a list of lists since this variable doesn't ever change at runtime and so immutability is expected for it * added docstrings and type hints for all the functions/class

--- karelics_vesc_can_driver/battery_status.py
+++ karelics_vesc_can_driver/battery_status.py
@@ -2,6 +2,7 @@
 from functools import partial
 import re
 import sys
+from typing import List
 
 import rclpy
 from rclpy.node import Node, NodeNameNonExistentError
@@ -11,6 +12,23 @@ from karelics_vesc_can_driver.msg import VescStatus
 
 
 class BatteryStatus(Node):
+    """
+    BatteryStatus node handling the publishing of the battery status according to the statuses of all the active vescs
+    in the system. The node dynamically subscribes to the status topics of each of the active vescs, gets the battery
+    voltages from them, computes the mean battery voltage and then publishes the overall battery status for the robot
+    under the /battery topic
+    """
+
+    # voltage, percentage.
+    # All the other voltage values between these key-values
+    # are linearly calculated
+    key_voltages = ((54.10, 1.0),
+                    (48, 0.6),
+                    (45.8, 0.2),
+                    (44.2, 0.05),
+                    (41.8, 0.02),
+                    (40, 0))
+
     def __init__(self):
         super().__init__("battery_status")
 
@@ -22,25 +40,37 @@ class BatteryStatus(Node):
 
         self.current_battery_voltage = None
 
-        # voltage, percentage. All the other voltage values between these key-values
-        # are linearly calculated
-        self.key_voltages = [[54.10, 1.0],
-                             [48, 0.6],
-                             [45.8, 0.2],
-                             [44.2, 0.05],
-                             [41.8, 0.02],
-                             [40, 0]]
+    def vesc_status_cb(self, data: VescStatus, vesc: str):
+        """
+        The Callback function for the vesc status subscribers
 
-    def vesc_status_cb(self, data, vesc):
+        :param VescStatus data: the data incoming from the vesc status topic
+        :param str vesc: the name of the vesc status we are subscribing to
+        """
+
         self.vesc_voltages[vesc] = float(data.v_in)
 
-    def get_mean_battery_voltage(self):
-        if len(self.vesc_voltages) != 0:
-            return float(sum(list(self.vesc_voltages.values())) / len(self.vesc_voltages))
-        else:
-            return 0.0
+    def get_mean_battery_voltage(self) -> float:
+        """
+        Computes the mean battery voltage from the values read from all the active vescs
 
-    def get_vesc_status_topics(self):
+        :return float mean_battery_voltage: the mean of the battery voltages read from the status topics of active vescs
+        """
+
+        mean_battery_voltage = 0.0
+
+        if len(self.vesc_voltages) != 0:
+            mean_battery_voltage = float(sum(list(self.vesc_voltages.values())) / len(self.vesc_voltages))
+
+        return mean_battery_voltage
+
+    def get_vesc_status_topics(self) -> List[str]:
+        """
+        Returns a list containing the status topics of all active vescs
+
+        :return List[str] vesc_status_topics: all the detected active vesc status topics in the system
+        """
+
         vesc_status_topics = []
 
         try:
@@ -58,10 +88,24 @@ class BatteryStatus(Node):
         return vesc_status_topics
 
     @staticmethod
-    def vesc_from_topic(topic):
+    def vesc_from_topic(topic: str) -> str:
+        """
+        Returns the vesc name from the status topic published by the vesc
+
+        :param str topic: the vesc status topic
+        :return: str: the name of the vesc decoded from the vesc status topic
+        """
+
         return list(filter(None, topic.split(sep='/')))[0]
 
-    def get_vescs_from_status_topics(self, topics_list):
+    def get_vescs_from_status_topics(self, topics_list: List[str]) -> List[str]:
+        """
+        Returns a list of vesc names from status topics
+
+        :param List[str] topics_list: list of status topics
+        :return List[str] active_vescs: list with the names of active vescs decoded from the status topics
+        """
+
         active_vescs = []
 
         for topic in topics_list:
@@ -69,19 +113,40 @@ class BatteryStatus(Node):
 
         return active_vescs
 
-    def create_new_status_sub(self, topic, vesc):
+    def create_new_status_sub(self, topic: str, vesc: str):
+        """
+        Creates a new subscription to the topic published by the specified vesc
+
+        :param str topic: status topic for which we want to create a new subscription
+        :param str vesc: name of the vesc publishing the status topic
+        """
+
         status_sub = self.create_subscription(VescStatus, topic, partial(self.vesc_status_cb,
                                                                          vesc=vesc),
                                               qos_profile=1)
         self.vesc_status_subscribers[vesc] = status_sub
 
-    def create_new_vesc_status_subs(self, topics_list):
+    def create_new_vesc_status_subs(self, topics_list: List[str]):
+        """
+        Creates new vesc status topic subs for all the newly detected active topics
+
+        :param List[str] topics_list: list of active vesc status topics
+        """
+
         for topic in topics_list:
             vesc_from_topic = self.vesc_from_topic(topic)
             if vesc_from_topic not in self.vesc_status_subscribers.keys():
                 self.create_new_status_sub(topic, vesc_from_topic)
 
-    def destroy_stale_vesc_status_subs(self, active_vescs):
+    def destroy_stale_vesc_status_subs(self, active_vescs: List[str]):
+        """
+        Goes through the subs we have already registered before to status topics and checks it against the list of
+        active vescs. If we detect that we have a sub to the status of a vesc that is no longer active in the system,
+        that subscription gets destroyed and removed from our dictionary of active subscriptions
+
+        :param List[str] active_vescs: list of names of the currently active vescs in the system
+        """
+
         vescs_to_destroy = []
         for vesc in list(self.vesc_status_subscribers.keys()):
             if vesc not in active_vescs:
@@ -93,6 +158,16 @@ class BatteryStatus(Node):
                 del self.vesc_voltages[vesc]
 
     def publish_battery_percentage(self):
+        """
+        Call back function for the timer responsible of publishing the battery status. On each timer callback (1.0
+        seconds at the moment) we get the status topics of all the vescs active in the system, we then create new
+        subscriptions for the newly discovered topics (published by vescs that were not active before), destroy and
+        delete stale subscriptions and then we get the mean battery voltage from the voltages reported by all the
+        active vescs and then publish it to the /battery topic. If we have no active vescs this function returns and
+        no data is then published on to the /battery topic.
+
+        :return None: only in the scenario when we have no active vescs detected in the system.
+        """
         active_vesc_status_topics = self.get_vesc_status_topics()
         active_vescs = self.get_vescs_from_status_topics(active_vesc_status_topics)
 
@@ -110,23 +185,32 @@ class BatteryStatus(Node):
         battery_state.percentage = self.get_battery_percentage(mean_battery_voltage)
         self.battery_pub.publish(battery_state)
 
-    def get_battery_percentage(self, curr_voltage):
-        last_index = len(self.key_voltages)
+    @staticmethod
+    def get_battery_percentage(curr_voltage: float):
+        """
+        Computes the battery percentage according to the voltage and the key static voltage limits
+        specified in BatteryStatus.key_voltages
+
+        :param float curr_voltage: voltage value
+        :return float percentage: percentage from voltage
+        """
+
+        last_index = len(BatteryStatus.key_voltages)
         for i in range(last_index):
-            upper_volt = self.key_voltages[i][0]
-            upper_percentage = self.key_voltages[i][1]
+            upper_volt = BatteryStatus.key_voltages[i][0]
+            upper_percentage = BatteryStatus.key_voltages[i][1]
 
             if i == last_index - 1:
                 return float(upper_percentage)
 
-            lower_volt = self.key_voltages[i + 1][0]
-            lower_percentage = self.key_voltages[i + 1][1]
+            lower_volt = BatteryStatus.key_voltages[i + 1][0]
+            lower_percentage = BatteryStatus.key_voltages[i + 1][1]
             if i == 0 and curr_voltage > upper_volt:
                 return float(upper_percentage)
             elif upper_volt > curr_voltage > lower_volt:
-                percentage = float((curr_voltage - lower_volt) / (upper_volt - lower_volt))
-                scaled_percentage = float(lower_percentage + (upper_percentage - lower_percentage) * percentage)
-                return scaled_percentage
+                percentage = (curr_voltage - lower_volt) / (upper_volt - lower_volt)
+                scaled_percentage = lower_percentage + (upper_percentage - lower_percentage) * percentage
+                return float(scaled_percentage)
 
 
 if __name__ == '__main__':
